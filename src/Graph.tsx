63fec936975c1c0ac7a310964a8820573cde6e6f
nikhilesh-927 <113814922+nikhilesh-927@users.noreply.github.com>
2023-10-06 15:45:36 +0000

    updated DataManipulator.ts and Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -18,43 +18,52 @@ class Graph extends Component<IProps, {}> {
     return React.createElement('perspective-viewer');
   }
 
-  componentDidMount() {
-    // Get element from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+   componentDidMount() {
+     // Get element from the DOM.
+     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+     //In below const schema "price_abc" and "price_def" are prices of stocks ABC and DEF
+     //"ratio" refers to ratio of ABC and DEF
+     //"upper_bound" and "lower_bound" when ratio corss one of these bounds then "trigger_alert"
+     //"timestamp" field since trackings are with respect to time
+     const schema = {
+       price_abc: 'float',
+       price_def: 'float',
+       ratio: 'float',
+       timestamp: 'date',
+       upper_bound: 'float',
+       lower_bound: 'float',
+       trigger_alert: 'float',
+     };
+    
+     if (window.perspective && window.perspective.worker()) {
+       this.table = window.perspective.worker().table(schema);
+     }
+     if (this.table) {
+       // Load the `table` in the `<perspective-viewer>` DOM reference.
+       elem.load(this.table);
+       elem.setAttribute('view', 'y_line');
+       elem.setAttribute('column-pivots', '["stock"]');
+       elem.setAttribute('row-pivots', '["timestamp"]');
+       elem.setAttribute('columns', '["top_ask_price"]');
+       elem.setAttribute('aggregates', JSON.stringify({
+         price_abc: 'avg',
+         price_def: 'avg',
+         ratio: 'avg',
+         timestamp: 'distinct count',
+         upper_bound: 'avg',
+         lower_bound: 'avg',
+         trigger_alert: 'avg',
+       }));
+     }
+   }
 
-    const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
-    };
-
-    if (window.perspective && window.perspective.worker()) {
-      this.table = window.perspective.worker().table(schema);
-    }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-      elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
-    }
-  }
-
-  componentDidUpdate() {
-    if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
-    }
-  }
-}
+   componentDidUpdate() {
+     if (this.table) {
+       this.table.update([
+         DataManipulator.generateRow(this.props.data),
+       ] as unknown as TableData);
+     }
+   }
+ }
 
 export default Graph;
